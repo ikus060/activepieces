a780125c05ea098a24985e3fbd03dd0ae7469aa1
Patrik Dufresne <patrik@ikus-soft.com>
2024-06-24 22:33:10 +0000

    feat: add more action to google contacts

--- packages/pieces/community/google-contacts/src/lib/common/index.ts
+++ packages/pieces/community/google-contacts/src/lib/common/index.ts
@@ -1,3 +1,16 @@
+import { ProcessorFn } from '@activepieces/pieces-framework';
+
 export const googleContactsCommon = {
   baseUrl: `https://people.googleapis.com/v1/people`,
+};
+
+export const processText: ProcessorFn<any, string> = (property, value) => {
+  // If empty string of false, return undefined to avoid updating contact.
+  if(value === null || value === undefined || value === false || value === '') {
+    return undefined;
+  }
+  if (typeof value === 'object') {
+    return JSON.stringify(value)
+  }
+  return value.toString();
 };
